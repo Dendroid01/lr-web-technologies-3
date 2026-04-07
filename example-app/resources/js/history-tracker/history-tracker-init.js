b13d81845f3755c0182d0fb64d1554a9da3fd8f7
@Dendroid01 <denis.gordienko06@gmail.com>
2026-04-07 19:03:24 +0300

    Restructurization of directory.

--- example-app/resources/js/history-tracker/history-tracker-init.js
+++
@@ -1,9 +0,0 @@
-import {HistoryTracker} from './tracker.js';
-import {HistoryUI} from './HistoryUI.js'
-
-$(document).ready(function () {
-    const currentPage = HistoryTracker.getCurrentPage();
-    HistoryTracker.trackPageVisit(currentPage);
-
-    HistoryUI.init();
-});
